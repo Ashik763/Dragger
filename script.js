fd98444e0fd3b1c4531173c2de4f2428e13e6fa0
Ashik Ghosh <ashikghosh763@gmail.com>
2023-06-09 03:48:13 +0600

    finally done

--- script.js
+++ script.js
@@ -1,65 +1,55 @@
-const img_containers = document.querySelectorAll('.img-container');
+// Left side images which will be dropped
+const img_containers = document.querySelectorAll(".img-container");
+
+// the drop section
 const right_img_container = document.getElementById("right-content");
 
-img_containers.forEach(img_container => {
-    console.dir(img_container);
-    img_container.addEventListener('dragstart', dragStart)
 
+img_containers.forEach((img_container) => {
+  img_container.addEventListener("dragstart", dragStart);
 });
 
-
-
 function dragStart(e) {
-    console.log("drag start");
-    e.dataTransfer.setData('text/plain', e.target.id);
-    setTimeout(() => {
-        // e.target.classList.add('hidden');
-    }, 0);
+ 
+  e.dataTransfer.setData("text/plain", e.target.id);
+
+  
 }
 
 
-right_img_container.addEventListener('dragenter', dragEnter)
-right_img_container.addEventListener('dragover', dragOver);
-right_img_container.addEventListener('dragleave', dragLeave);
-right_img_container.addEventListener('drop', drop);
-
-
+// Adding event listeners to the drop section
+right_img_container.addEventListener("dragenter", dragEnter);
+right_img_container.addEventListener("dragover", dragOver);
+right_img_container.addEventListener("dragleave", dragLeave);
+right_img_container.addEventListener("drop", drop);
 
 function dragEnter(e) {
-    e.preventDefault(e);
-  
+  e.preventDefault(e);
 
-    
-    e.target.classList.add('dragging');
+  e.target.classList.add("dragging");
 }
 
 function dragOver(e) {
-    e.preventDefault();
-    e.target.classList.add('dragging');
+  e.preventDefault();
+  e.target.classList.add("dragging");
 }
 
 function dragLeave(e) {
-    // e.preventDefault();
-    e.target.classList.remove('dragging');
+  e.target.classList.remove("dragging");
 }
 
 function drop(e) {
-    // e.preventDefault();
-    console.log("drop");
-    e.target.classList.remove('dragging');
-    const id = e.dataTransfer.getData('text/plain');
-    console.log(id);
-    const draggable = document.getElementById(id);
-    // e.target.appendChild(draggable);
-    console.log( e.target.parentNode);
-    e.target.parentNode.appendChild(draggable);
+ 
+  e.target.classList.remove("dragging");
+  const id = e.dataTransfer.getData("text/plain");
+  const dragged = document.getElementById(id);
 
-    // display the draggable element
-    draggable.classList.remove('hidden');
+  e.target.parentNode.appendChild(dragged);
+  dragged.classList.remove("hidden");
 }
 
 
-function handleReset(){
-   
-    location.reload();
+//  Reset Button
+function handleReset() {
+  location.reload();
 }
